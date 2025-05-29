71375ca583a2494021059e79ad13f00673f38bbb
hiHarish <156064772+hiHarish@users.noreply.github.com>
2025-05-29 16:36:39 +0530

    Update Customers.jsx

--- customer support/src/Pages/Customers/Customers.jsx
+++ customer support/src/Pages/Customers/Customers.jsx
@@ -12,7 +12,7 @@ const Customer = () => {
   useEffect(() => {
     const fetchOrders = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/api/orders");
+        const response = await axios.get("https://backend-7j5d.onrender.com/api/orders");
         if (response.data.success) {
           setOrders(response.data.data);
         } else {
